f5932af7db09f32a3f8fd4770893929b642d05d3
ark930 <lark930@gmail.com>
2018-10-03 15:44:23 +0800

    commit new examples

--- 33_struct-fields.go
+++ 33_struct-fields.go
@@ -3,10 +3,12 @@ package main
 import "fmt"
 
 type Vertex struct {
-    X int
-    Y int
+	X int
+	Y int
 }
 
 func main() {
-    fmt.Println(Vertex{1,2})
+	v := Vertex{1, 2}
+	v.X = 4
+	fmt.Println(v.X)
 }
